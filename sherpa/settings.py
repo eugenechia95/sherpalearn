8f8fa630f411437aef74a95284c7cfff8a0edca4
eugenechia95 <39354695+eugenechia95@users.noreply.github.com>
2018-10-28 18:10:26 +0800

    New Files

--- sherpa/settings.py
+++ sherpa/settings.py
@@ -20,7 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uihmzsoh#9y%n&e$=hqy#ozr5zc8^y0qh3#55r!1y*yglf$@ii'
+#SECRET_KEY = 'uihmzsoh#9y%n&e$=hqy#ozr5zc8^y0qh3#55r!1y*yglf$@ii'
+SECRET_KEY = config('uihmzsoh#9y%n&e$=hqy#ozr5zc8^y0qh3#55r!1y*yglf$@ii')
+#new
+DEBUG = config('DEBUG', default=False, cast=bool)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -125,3 +133,18 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+#NEW STUFF
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
